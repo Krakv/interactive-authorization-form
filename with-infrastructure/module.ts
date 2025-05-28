77eb361d8c16761ac6c134b0c70c215a881e5916
Igor Tokmakov <104667117+Krakv@users.noreply.github.com>
2025-05-28 11:16:15 +0500

    Changed Password

--- with-infrastructure/module.ts
+++ with-infrastructure/module.ts
@@ -18,7 +18,7 @@ export function validateForm(emailInput : HTMLInputElement, passwordElement : HT
 
 export function check(emailElement : HTMLInputElement, passwordElement : HTMLInputElement, message : HTMLDivElement) {
     const correctEmail = 'test@test.ru'
-    const correctPassword = 'difficultPassword2025'
+    const correctPassword = 'easyPassword1'
 
     const email = emailElement.value
     const password = passwordElement.value
