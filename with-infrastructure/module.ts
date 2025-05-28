21f8a26e252f56cf921437e025b3c791d0f06155
Igor Tokmakov <104667117+Krakv@users.noreply.github.com>
2025-05-28 11:45:28 +0500

    Update module.ts

--- with-infrastructure/module.ts
+++ with-infrastructure/module.ts
@@ -18,7 +18,7 @@ export function validateForm(emailInput : HTMLInputElement, passwordElement : HT
 
 export function check(emailElement : HTMLInputElement, passwordElement : HTMLInputElement, message : HTMLDivElement) {
     const correctEmail = 'test@test.ru'
-    const correctPassword = 'easyPassword1'
+    const correctPassword = 'easyPassword12'
 
     const email = emailElement.value
     const password = passwordElement.value
