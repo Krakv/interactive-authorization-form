234c353099e6f99e30d4ba4d298c24d2e58e1503
Igor Tokmakov <104667117+Krakv@users.noreply.github.com>
2025-06-06 12:41:21 +0500

    Created react project

---
+++ framework/src/test/authentification.test.ts
@@ -0,0 +1,45 @@
+import { check } from '../modules/module';
+
+document.body.innerHTML = `
+  <input type="email" id="email" value="" />
+  <input type="password" id="password" value="" />
+  <div id="message"></div>
+`;
+
+describe('check', () => {
+  it('should return true and display Welcome message', () => {
+    const emailInput = document.getElementById('email') as HTMLInputElement;
+    emailInput.value = 'idtokmakov@edu.hse.ru';
+    const passwordInput = document.getElementById('password') as HTMLInputElement;
+    passwordInput.value = 'difficultpassword2025';
+    const messageElement = document.getElementById('message') as HTMLDivElement;
+
+    const result = check(emailInput, passwordInput, messageElement);
+    expect(result).toBe(true);
+    expect(messageElement.textContent).toBe('Добро пожаловать!');
+  });
+
+  it('should return false and display error message for wrong email', () => {
+    const emailInput = document.getElementById('email') as HTMLInputElement;
+    emailInput.value = 'test@example.com';
+    const passwordInput = document.getElementById('password') as HTMLInputElement;
+    passwordInput.value = 'difficultpassword2025';
+    const messageElement = document.getElementById('message') as HTMLDivElement;
+
+    const result = check(emailInput, passwordInput, messageElement);
+    expect(result).toBe(false);
+    expect(messageElement.textContent).toBe('Неверный логин или пароль');
+  });
+
+  it('should return false and display error message for wrong password', () => {
+    const emailInput = document.getElementById('email') as HTMLInputElement;
+    emailInput.value = 'idtokmakov@edu.hse.ru';
+    const passwordInput = document.getElementById('password') as HTMLInputElement;
+    passwordInput.value = '123456';
+    const messageElement = document.getElementById('message') as HTMLDivElement;
+
+    const result = check(emailInput, passwordInput, messageElement);
+    expect(result).toBe(false);
+    expect(messageElement.textContent).toBe('Неверный логин или пароль');
+  });
+});
